c8b45a259ff3e454d5cdd93968ecdb9af5596e3c
Matt Button <matt.button@geckoboard.com>
2019-01-09 15:29:29 +0000

    Add more rules for extracting tags from envoy's metrics
    https://github.com/envoyproxy/envoy/blob/87553968ec2258919d986e9a76512b0009d01575/source/common/config/well_known_names.cc

--- rules.go
+++ rules.go
@@ -26,10 +26,14 @@ var rules = []dsl.Rule{
 	},
 	{
 		RequiredPrefix: "envoy.",
-		PartialMatch:   "listener.{envoy_listener}.http.",
-		ReplaceWith:    "listener.http.",
+		PartialMatch:   "listener.{envoy_listener}.",
+		ReplaceWith:    "listener.",
 		CustomPatterns: map[string]*regexp.Regexp{
-			"envoy_listener": regexp.MustCompile(`.+`),
+			// Inspired by https://github.com/envoyproxy/envoy/blob/87553968ec2258919d986e9a76512b0009d01575/source/common/config/well_known_names.cc#L102
+			// My understanding is that this matches an ip/port combo,
+			// or a UUID?
+			// https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/lds.proto#envoy-api-msg-listener
+			"envoy_listener": regexp.MustCompile(`(?:[\d\._]+|[_\[\]aAbBcCdDeEfF\d]+)`),
 		},
 	},
 	{
@@ -47,5 +51,10 @@ var rules = []dsl.Rule{
 		CustomPatterns: map[string]*regexp.Regexp{
 			"envoy_http_status_class": regexp.MustCompile(`(?i)\dxx`),
 		},
+	},
+	{
+		RequiredPrefix: "envoy.",
+		PartialMatch:   "ssl.ciphers.{envoy_ssl_cipher}",
+		ReplaceWith:    "ssl.ciphers",
 	},
 }
